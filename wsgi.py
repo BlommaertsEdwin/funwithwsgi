6158163e8d2560292b262d5f2713637ec8850745
Edwin Blommaerts <edwin.blommaerts@gmail.com>
2014-06-17 01:00:18 +0100

    Added method to parse htdocs directory, returns a dictionary with key, pair as htmldoc na,e (without extension) and full path (including file name and etension)

--- wsgi.py
+++ wsgi.py
@@ -3,19 +3,21 @@
 __author__ = 'edwin'
 import os
 import mimetypes
+from os.path import isfile
 
 
 def application(environ, start_response):
 
     working_directory = os.getcwd()
-    htdocs = "/htdocs"
+    htdocs_dir = "/htdocs"
     index_file = "/index.html"
     static_directory = "/static"
     path = environ.get('PATH_INFO')
 
+    #TODO: refactor this method to use the parse method instead of hardcoded values
     if environ['PATH_INFO'] == '/about':
         file = "/about.html"
-        full_path = "".join([working_directory, htdocs, file])
+        full_path = "".join([working_directory, htdocs_dir, file])
         return_value = read_file(full_path)
         start_response('200 OK', [('Content-Type', guess_mimetype("".join([path, file])))])
         return [return_value]
@@ -28,15 +30,16 @@ def application(environ, start_response):
         start_response('200 OK', [('Content-type', guess_mimetype(path))])
         return [open("".join([working_directory, environ['PATH_INFO']])).read()]
     else:
-        full_path = "".join([working_directory, htdocs, index_file])
+        full_path = "".join([working_directory, htdocs_dir, index_file])
         return_value = read_file(full_path)
         start_response('200 OK', [('Content-Type', guess_mimetype("".join([path, index_file])))])
+        print parse_htdocs_directory(working_directory, htdocs_dir)
         return [return_value]
 
 
 def read_file(path):
-    with open(path) as f:
-        data = f.read()
+    with open(path) as file:
+        data = file.read()
     return data
 
 
@@ -44,6 +47,14 @@ def guess_mimetype(path):
     mimetypes.init()
     return mimetypes.guess_type(path)[0]
 
+def parse_htdocs_directory(path, htdocs_dir):
+    htdocs_files = {}
+    htdocs_path = "".join([path, htdocs_dir])
+    for file in os.listdir(htdocs_path):
+        file_path = "".join([htdocs_path, "/", file])
+        if isfile(file_path):
+            htdocs_files[os.path.splitext(file)[0]] = file_path
+    return htdocs_files
 
 
 
